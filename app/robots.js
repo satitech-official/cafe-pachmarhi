81165edd7f6d9b69e0eced6ca7bd41a7692dd2fe
SatiTech <www.satitechinfo@gmail.com>
2026-08-24 15:49:32 +0530

    Make robots metadata static for Pages export

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,8 @@
+export const dynamic = 'force-static';
+
 export default function robots() {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: '/sitemap.xml',
+    sitemap: 'https://satitech-official.github.io/cafe-pachmarhi/sitemap.xml',
   };
 }
