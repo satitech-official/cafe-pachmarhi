4b4a6513cf324ce0d54aa1c382fbd066a0a6a23f
SatiTech <www.satitechinfo@gmail.com>
2026-08-24 15:49:43 +0530

    Make sitemap metadata static for Pages export

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,11 @@
+export const dynamic = 'force-static';
+
 const pages = ['', '/about/', '/menu/', '/featured/', '/offers/', '/gallery/', '/reviews/', '/blog/', '/contact/'];
 
 export default function sitemap() {
   const base = 'https://satitech-official.github.io/cafe-pachmarhi';
   return pages.map((path) => ({
     url: `${base}${path || '/'}`,
-    lastModified: new Date(),
     changeFrequency: 'monthly',
     priority: path ? 0.7 : 1,
   }));
